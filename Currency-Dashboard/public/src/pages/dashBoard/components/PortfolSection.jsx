40673b235a501bd1795cef97b9a9c48d19d34a93
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-08-26 00:17:58 +0530

    Update PortfolSection.jsx

--- Currency-Dashboard/public/src/pages/dashBoard/components/PortfolSection.jsx
+++ Currency-Dashboard/public/src/pages/dashBoard/components/PortfolSection.jsx
@@ -4,6 +4,7 @@ import { IoIosInformationCircleOutline } from "react-icons/io";
 import { FaArrowDownLong } from "react-icons/fa6";
 import { FaArrowUpLong } from "react-icons/fa6";
 
+
 const PortfolSection = () => {
     return (
         <HStack bg="white" borderRadius="xl" p="6" justify="space-between"
@@ -56,3 +57,4 @@ const PortfolSection = () => {
 }
 
 export default PortfolSection
+
