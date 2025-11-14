f534ebf14a65332d94a087ef851425224752bb8f
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-11-15 02:23:26 +0530

    Update App.jsx

--- Currency-Dashboard/public/src/App.jsx
+++ Currency-Dashboard/public/src/App.jsx
@@ -4,7 +4,6 @@ import DashBoard from './pages/dashBoard/DashBoard'
 import Transaction from '../src/pages/transcation/Transaction.jsx'
 import Support from '../src/pages/support/Support.jsx'
 
-
 function App() {
   const router = createBrowserRouter([
     {
