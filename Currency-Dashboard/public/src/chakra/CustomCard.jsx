999ddf7db1485c9e4bdaac15bdb75775ddb15707
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-11-24 01:33:14 +0530

    Update CustomCard.jsx

--- Currency-Dashboard/public/src/chakra/CustomCard.jsx
+++ Currency-Dashboard/public/src/chakra/CustomCard.jsx
@@ -1,7 +1,14 @@
 import { chakra } from "@chakra-ui/react";
 
+
+
+
+
+
+
  const CustomCard = chakra("div", {
     baseStyle: { bg: "white", borderRadius: "xl", p: "6" },
 });
+
 
 export default CustomCard
